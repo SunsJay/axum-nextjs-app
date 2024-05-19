0af78c20a74ff7854206a2dcdf247f6964c11a40
Suns Jay <sunsjay0806@gmail.com>
2024-05-19 21:41:40 +0800

    feat: add axum

--- backend/crates/core/main.rs
+++ backend/crates/core/main.rs
@@ -1,5 +1,13 @@
-fn main() {
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
     println!("🚀 Axum - NextJS - App v1.0");
     println!("🧓 SunsJay <sunsjay0806@gmail.com>");
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
