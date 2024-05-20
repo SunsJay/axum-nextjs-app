f6a54d404b9033f9b9b455a71812dfadbdcf2a69
Suns Jay <sunsjay0806@gmail.com>
2024-05-20 16:18:41 +0800

    perf: improve import

--- backend/main.rs
+++ backend/main.rs
@@ -1,9 +1,9 @@
-use route;
+use route::route;
 #[tokio::main]
 async fn main() {
     println!("🚀 Axum - NextJS - App v1.0");
     println!("🧓 SunsJay <sunsjay0806@gmail.com>");
-    let app = route::route();
+    let app = route();
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
