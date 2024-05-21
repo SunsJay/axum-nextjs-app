9f3ad1f9d0549db85616350f61f57c66e959185a
Suns Jay <sunsjay0806@gmail.com>
2024-05-21 15:35:03 +0800

    feat: handler undefined route

--- backend/route/src/lib.rs
+++ backend/route/src/lib.rs
@@ -1,6 +1,11 @@
-
+use axum::http::StatusCode;
 use axum::{routing::get, Router};
-
 pub fn route() -> Router {
-      Router::new().route("/", get(|| async { "Hello, World!" }))
+    Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .fallback(fallback)
+}
+
+pub async fn fallback() -> (StatusCode, &'static str) {
+    (StatusCode::NOT_FOUND, "Not Found")
 }
