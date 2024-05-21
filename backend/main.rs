0c55f7aaf7d053417d7929f8b69bac5a46a1459e
Suns Jay <sunsjay0806@gmail.com>
2024-05-21 17:46:01 +0800

    refactor: refactor route

--- backend/main.rs
+++ backend/main.rs
@@ -5,7 +5,6 @@ async fn main() {
     println!("🚀 Axum - NextJS - App v1.0");
     println!("🧓 SunsJay <sunsjay0806@gmail.com>");
     let app = route();
-    //    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     match tokio::net::TcpListener::bind("0.0.0.0:3000").await {
         Ok(listener) => axum::serve(listener, app).await.unwrap(),
         Err(e) => {
--- backend/route/src/lib.rs
+++ backend/route/src/lib.rs
@@ -1,10 +1,14 @@
 use axum::http::StatusCode;
 use axum::{routing::get, Router};
+
 pub fn route() -> Router {
-    Router::new()
-        .route("/", get(|| async { "Hello, World!" }))
-        .fallback(fallback)
+    let user_routes = Router::new().route("/:id", get(|| async { "User" }));
+    let api_routes = Router::new()
+        .nest("/users", user_routes);
+
+    Router::new().route("/", get(|| async { "Hello, World!" })).nest("/v1/api", api_routes).fallback(fallback)
 }
+
 
 pub async fn fallback() -> (StatusCode, &'static str) {
     (StatusCode::NOT_FOUND, "Not Found")
