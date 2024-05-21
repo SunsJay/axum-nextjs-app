fb71f71ebc9661348f82e2c33e4c12ab8e4cad72
Suns Jay <sunsjay0806@gmail.com>
2024-05-21 16:24:49 +0800

    feat: add print tcp listener error

--- backend/main.rs
+++ backend/main.rs
@@ -1,9 +1,16 @@
 use route::route;
+
 #[tokio::main]
 async fn main() {
     println!("🚀 Axum - NextJS - App v1.0");
     println!("🧓 SunsJay <sunsjay0806@gmail.com>");
     let app = route();
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    //    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    match tokio::net::TcpListener::bind("0.0.0.0:3000").await {
+        Ok(listener) => axum::serve(listener, app).await.unwrap(),
+        Err(e) => {
+            println!("{}", e);
+        }
+
+    }
 }
